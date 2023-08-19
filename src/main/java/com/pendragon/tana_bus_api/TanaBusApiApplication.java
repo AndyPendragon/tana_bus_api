586ccc835e88c15233ae37b0c94ca9950fefb311
AndyPendragon <116746292+AndyPendragon@users.noreply.github.com>
2023-08-19 11:24:35 +0300

    feat: adding local folder to repository

---
+++ src/main/java/com/pendragon/tana_bus_api/TanaBusApiApplication.java
@@ -0,0 +1,13 @@
+package com.pendragon.tana_bus_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanaBusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TanaBusApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pendragon/tana_bus_api/TanaBusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pendragon.tana_bus_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TanaBusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
