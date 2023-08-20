850034c663b1cf13b040d3bc31bf2fcad4a192a1
AndyPendragon <116746292+AndyPendragon@users.noreply.github.com>
2023-08-20 10:22:23 +0300

    feat: add stop controller

---
+++ src/main/java/com/pendragon/tana_bus_api/controller/StopController.java
@@ -0,0 +1,24 @@
+package com.pendragon.tana_bus_api.controller;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pendragon.tana_bus_api.entity.Stop;
+import com.pendragon.tana_bus_api.service.StopService;
+
+@RestController
+public class StopController {
+    private StopService service;
+
+    public StopController(StopService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/stops")
+    public List<Stop> getAllStops() throws SQLException{
+        return service.getAllStops();
+    }
+}
