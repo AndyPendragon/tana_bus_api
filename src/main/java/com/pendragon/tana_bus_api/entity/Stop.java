174aa2b84a0f4b175fd02ead275951d794a2277b
AndyPendragon <116746292+AndyPendragon@users.noreply.github.com>
2023-08-23 20:44:12 +0300

    fix: add NoArgs, NonNull, RequiredArgs to Stop

--- src/main/java/com/pendragon/tana_bus_api/entity/Stop.java
+++ src/main/java/com/pendragon/tana_bus_api/entity/Stop.java
@@ -3,19 +3,26 @@ package com.pendragon.tana_bus_api.entity;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
 @EqualsAndHashCode
 @Getter
 @Setter
 @ToString
 public class Stop {
-    private int stop_id;
+    private Integer stop_id;
+    @NonNull
     private String name;
     private double longitude;
     private double latitude;
-    private String location_id;
+    @NonNull
+    private Integer location_id;
     private int next_stop_id;
 }
