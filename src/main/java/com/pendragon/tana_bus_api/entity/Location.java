70dcf638c337e069dc4b22a4b769416997f911b9
AndyPendragon <116746292+AndyPendragon@users.noreply.github.com>
2023-08-24 08:40:39 +0300

    feat: add location entity

--- src/main/java/com/pendragon/tana_bus_api/entity/Location.java
+++ src/main/java/com/pendragon/tana_bus_api/entity/Location.java
@@ -3,9 +3,11 @@ package com.pendragon.tana_bus_api.entity;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
 @Getter
